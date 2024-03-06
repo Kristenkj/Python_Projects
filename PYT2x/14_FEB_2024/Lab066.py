c59ac9ce3c2cafb6f1a824d3094152c9cff17226
Kristenkj <jonesbkristen@gmail.com>
2024-03-06 14:32:10 -0500

    3/6/2024 Commit

--- PYT2x/14_FEB_2024/Lab066.py
+++ PYT2x/14_FEB_2024/Lab066.py
@@ -1,3 +1,7 @@
 my_list = [True, "Pramod", 12.34, 90] # TypeError: '<' not supported between instances
 my_list.sort()
 print(my_list)
+
+"""
+Sorting is not allowed with different types of data
+"""
